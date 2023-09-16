0f07f0484e141322c97e933e9cdb864607e359aa
Xin Wei <xwsze@synagie.com>
2023-09-16 23:07:19 +0800

    Created config file

---
+++ config/config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDRXj87Z1bYheVTJ2nDFwpwJSR06Y6PLd4",
+  authDomain: "espeeyam7.firebaseapp.com",
+  databaseURL: "https://espeeyam7-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "espeeyam7",
+  storageBucket: "espeeyam7.appspot.com",
+  messagingSenderId: "161221372506",
+  appId: "1:161221372506:web:7de79c31db30220002c663",
+  measurementId: "G-SS6GV95ZHC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
