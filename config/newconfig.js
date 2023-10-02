13e6c6b4169258d7a5dd4f8d0f5ad50c6ff8a0e3
Xin Wei <xwsze@synagie.com>
2023-10-02 17:06:09 +0800

    added gitignore

--- config/newconfig.js
+++ config/newconfig.js
@@ -1,20 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import firebaseConfig from "/config/config.js"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDRXj87Z1bYheVTJ2nDFwpwJSR06Y6PLd4",
-  authDomain: "espeeyam7.firebaseapp.com",
-  databaseURL: "https://espeeyam7-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "espeeyam7",
-  storageBucket: "espeeyam7.appspot.com",
-  messagingSenderId: "161221372506",
-  appId: "1:161221372506:web:7de79c31db30220002c663",
-  measurementId: "G-SS6GV95ZHC"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
