d739479380115c0ecc7714c02dda07a8e7591c6c
Xin Wei <xinweisze@gmail.com>
2023-10-11 18:03:08 +0800

    Search function for skills connected to db

--- Admin UI/create_listing.js
+++ Admin UI/create_listing.js
@@ -1,46 +1,75 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const separator = ',';
+import app from '../config/newconfig.js';
+import { getDatabase, set, ref, update, get, child, push } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+import { getStorage, ref as sRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-storage.js";
 
-    for (const input of document.getElementsByTagName("input")) {
-        if (!input.multiple) {
-            continue;
-        }
+const database = getDatabase(app);
+const auth = getAuth(app);
+const storage = getStorage()
+const storageref = sRef(storage);
 
-        if (input.list instanceof HTMLDataListElement) {
-            const optionsValues = Array.from(input.list.options).map(opt => opt.value);
-            let valueCount = input.value.split(separator).length;
+let skills = [];
 
-            input.addEventListener("input", () => {
-                const currentValueCount = input.value.split(separator).length;
-
-                // Do not update list if the user doesn't add/remove a separator
-                // Current value: "a, b, c"; New value: "a, b, cd" => Do not change the list
-                // Current value: "a, b, c"; New value: "a, b, c," => Update the list
-                // Current value: "a, b, c"; New value: "a, b" => Update the list
-                if (valueCount !== currentValueCount) {
-                    const lsIndex = input.value.lastIndexOf(separator);
-                    const str = lsIndex !== -1 ? input.value.substr(0, lsIndex) + separator : "";
-                    filldatalist(input, optionsValues, str);
-                    valueCount = currentValueCount;
-                }
-            });
-        }
+// Define a function to fetch skills from the Firebase database
+async function fetchSkillsFromFirebase() {
+  const skillsRef = ref(database, 'Skills'); // Use the appropriate reference
+  try {
+    const snapshot = await get(skillsRef);
+    if (snapshot.exists()) {
+      const skillsData = snapshot.val();
+      skills = Object.values(skillsData);
+    } else {
+      console.log('No skills data available.');
     }
+  } catch (error) {
+    console.error('Error fetching skills data:', error);
+  }
+}
 
-    function filldatalist(input: HTMLInputElement, optionValues: string[], optionPrefix: string) {
-        const list = input.list;
-        if (list && optionValues.length > 0) {
-            list.innerHTML = "";
+// Define a function to populate the shared datalist
+function populateDropdown() {
+  const skillsDropdown = document.getElementById('skills');
+  skillsDropdown.innerHTML = '';
+  skills.forEach(skill => {
+    const option = document.createElement('option');
+    option.value = skill;
+    skillsDropdown.appendChild(option);
+  });
+}
 
-            const usedOptions = optionPrefix.split(separator).map(value => value.trim());
+// Define a function to handle search input for all input elements
+function handleSearchInput() {
+  const inputElements = document.querySelectorAll('input[list="skills"]');
+  inputElements.forEach(inputElement => {
+    const searchTerm = inputElement.value.charAt(0).toLowerCase();
+    // The rest of the code for handling input and filtering
+    // For example, you can filter skills starting with searchTerm
+    const filteredSkills = skills.filter(skill =>
+      skill.toLowerCase().startsWith(searchTerm)
+    );
+    // Then, you can update the displayed options in the shared datalist
+    const skillsDropdown = document.getElementById('skills');
+    skillsDropdown.innerHTML = '';
+    filteredSkills.forEach(skill => {
+      const option = document.createElement('option');
+      option.value = skill;
+      skillsDropdown.appendChild(option);
+    });
+  });
+}
 
-            for (const optionsValue of optionValues) {
-                if (usedOptions.indexOf(optionsValue) < 0) { // Skip used values
-                    const option = document.createElement("option");
-                    option.value = optionPrefix + optionsValue;
-                    list.append(option);
-                }
-            }
-        }
-    }
+// Add event listeners and fetch data
+document.querySelectorAll('input[list="skills"]').forEach(inputElement => {
+  inputElement.addEventListener('input', handleSearchInput);
 });
+
+fetchSkillsFromFirebase()
+  .then(() => populateDropdown())
+  .catch(error => console.error('Error:', error));
+
+
+const jobsRef = ref(database, 'jobs')
+const jobTitle = document.getElementById('jobTitle').value
+const details = document.getElementById('jobDescription').value
+const location = document.getElementById('jobLocation').value
+const department = document.getElementById('jobDept').value
