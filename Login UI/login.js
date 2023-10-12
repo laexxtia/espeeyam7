d365251e8a570a58cd4219a8a5b193b9173a5a2d
laexxtia <laetitiahongje@gmail.com>
2023-10-12 16:17:32 +0800

    login mid-bug

--- Login UI/login.js
+++ Login UI/login.js
@@ -10,6 +10,22 @@ const storageref = sRef(storage);
 
 let login = document.getElementById('submit')
 
+let userData
+
+async function getUserDataFromFirebase(user_ref) {
+    try {
+        const snapshot = await get(user_ref); // Retrieve data from the Firebase Realtime Database using the 'user_ref' you defined
+        if (snapshot.exists()) {
+            userData = snapshot.val(); // Assign the retrieved data to the 'userData' variable
+        } else {
+            userData = null; // Set 'userData' to null or another appropriate value when no data exists
+        }
+    } catch (error) {
+        console.error("Error fetching user data:", error);
+        throw error;
+    }
+}
+
 if (login != null) {
     login.addEventListener("click", async function() { // Note the 'async' keyword
         const inputEmail = document.getElementById('email').value;
@@ -29,8 +45,24 @@ if (login != null) {
                 onAuthStateChanged(auth, (user) => {
                     if (user) {
                       // User is signed in, redirect to authenticated page
-                      setPersistence(auth, browserSessionPersistence)
-                      window.location.href = '/Staff UI/job_listing.html';
+                      const userUID = user.uid
+                      const user_ref = ref(database, 'Staff/' + userUID)
+                      getUserDataFromFirebase(user_ref)
+                      console.log(userUID)
+                      console.log(userData)
+                    
+
+                    
+                      
+                      if(userRole == '1'){
+                        setPersistence(auth, browserSessionPersistence)
+                        window.location.href = '/Staff UI/job_listing.html';
+                      }
+                      else{
+                        setPersistence(auth, browserSessionPersistence)
+                        window.location.href = '/Admin UI/job_listing.html';
+                      }
+
                     } else {
                       // User is signed out, redirect to login page
                       console.log("user is not signed in");
