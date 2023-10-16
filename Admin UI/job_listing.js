4ac66fa5a075cddbd6be321ef605b9c8dbf47507
Xin Wei <xinweisze@gmail.com>
2023-10-16 18:31:03 +0800

    oiafioh

--- Admin UI/job_listing.js
+++ Admin UI/job_listing.js
@@ -1,140 +1,3 @@
-// const jobs = [
-//     {
-//         title: "Software Engineer",
-//         image: "images/software-engineer.png",
-//         details:
-//         "Responsible for designing, developing and maintaining software systems and applications.",
-//         // openPositions: "2",
-//         link: "role_listing.html",
-//         appLink: "applicant_list.html"
-//     },
-    
-//     {
-//         title: "Data Scientist",
-//         image: "images/data-scientist.png",
-//         details:
-//         "Responsible for collecting, analyzing and interpreting large data sets to help organizations make better decisions.",
-//         // openPositions: "3",
-//         link: "#",
-//         appLink: "applicant_list.html"
-//     },
-    
-//     {
-//         title: "Project Manager",
-//         image: "images/project-manager.png",
-//         details:
-//         "Responsible for planning, executing and closing projects on time and within budget.",
-//         // openPositions: "1",
-//         link: "#",
-//         appLink: "applicant_list.html"
-//     },
-    
-//     {
-//         title: "Product Manager",
-//         image: "images/product-manager.png",
-//         details:
-//         "Responsible for managing the entire product life cycle, from ideation to launch and post-launch maintenance.",
-//         // openPositions: "1",
-//         link: "#",
-//         appLink: "applicant_list.html"
-//     },
-    
-//     {
-//         title: "Sales Representative",
-//         image: "images/sales-representative.png",
-//         details:
-//         "Responsible for reaching out to potential customers and closing sales deals.",
-//         // openPositions: "4",
-//         link: "#",
-//         appLink: "applicant_list.html"
-//     },
-
-// ]
-
-// const jobsHeading = document.querySelector(".container-fluid h2");
-// const jobsMainContainer = document.querySelector(".jobs-list-container")
-// const jobSearch = document.querySelector(".form-control")
-
-// let searchTerm ="";
-
-
-// if (jobs.length==1){
-//     jobsHeading.innerHTML = `${jobs.length} Job`; 
-// } else if(jobs.length == 0){
-//     jobsHeading.innerHTML = `No Jobs`
-// } else{
-//     jobsHeading.innerHTML=`${jobs.length} Jobs`
-// }
-
-// function createJobListingCards() {
-//     let jobsContainer = document.createElement("div");
-//     jobsContainer.setAttribute("class", "row mx-auto");
-
-//     jobsContainer.innerHTML="";
-//     jobsMainContainer.innerHTML="";
-
-//     let matchingJobsCount = 0;
-
-//     jobs.forEach(job => {
-//         // console.log(job.title);
-//         if(job.title.toLowerCase().includes(searchTerm.toLowerCase())){
-
-//             let jobCard = document.createElement("div");
-//             jobCard.setAttribute("class", "col-3 box") 
-
-//             let image = document.createElement("img");
-//             image.src = job.image
-//             image.setAttribute("width", "30px")
-//             image.setAttribute("height", "30px")
-
-
-//             let title = document.createElement("h3");
-//             title.classList.add("job-title");
-//             title.innerText = job.title;
-
-//             let details = document.createElement("div");
-//             details.classList.add("details");
-//             details.innerText = job.details;
-
-//             let detailsbtn = document.createElement("a")
-//             detailsbtn.setAttribute("class", "btn btn-primary")
-//             detailsbtn.innerHTML = "More Details";
-//             detailsbtn.href = job.link;
-
-//             let applicantsbtn = document.createElement("a")
-//             applicantsbtn.setAttribute("class", "btn btn-primary applicantsbtn")
-//             applicantsbtn.innerHTML = "View Applicants";
-//             applicantsbtn.href = job.appLink;
-
-//             jobCard.appendChild(image);
-//             jobCard.appendChild(title);
-//             jobCard.appendChild(details);
-//             jobCard.appendChild(detailsbtn);
-//             jobCard.appendChild(applicantsbtn)
-
-//             jobsContainer.append(jobCard)
-            
-//             jobsMainContainer.append(jobsContainer)
-//             matchingJobsCount++
-//         }
-//         if (matchingJobsCount === 1) {
-//             jobsHeading.innerHTML = `${matchingJobsCount} Job`;
-//         } else if(matchingJobsCount == 0){
-//             jobsHeading.innerHTML = `No Jobs`
-//         } else{
-//             jobsHeading.innerHTML=`${jobs.length} Jobs`
-//         }
-    
-//     })
-// }
-
-// createJobListingCards();
-
-// jobSearch.addEventListener("input",(e) =>{
-//     searchTerm=e.target.value;
-//     createJobListingCards();
-// })
-
 import app from '../config/newconfig.js';
 import { getDatabase, set, ref, update, get, child, push } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
@@ -232,11 +95,6 @@ auth.onAuthStateChanged(function (user) {
             }
         }
         
-
-
-
-
-
         // To use the function and get the jobs data:
         async function main() {
             try {
@@ -267,10 +125,10 @@ auth.onAuthStateChanged(function (user) {
 
                             // ... Create job card content as before ...
 
-                            let image = document.createElement("img");
-                            image.src = job.image
-                            image.setAttribute("width", "30px")
-                            image.setAttribute("height", "30px")
+                            // let image = document.createElement("img");
+                            // image.src = job.image
+                            // image.setAttribute("width", "30px")
+                            // image.setAttribute("height", "30px")
 
 
                             let title = document.createElement("h3");
@@ -287,34 +145,34 @@ auth.onAuthStateChanged(function (user) {
                             detailsbtn.innerHTML = "More Details";
                             detailsbtn.href = job.link;
 
-                            jobCard.appendChild(image);
+                            // jobCard.appendChild(image);
                             jobCard.appendChild(title);
                             jobCard.appendChild(details);
 
-                            let skill_row = document.createElement("div")
-                            skill_row.setAttribute("class", "skillrow")
-                            let skill_text = document.createElement("p")
-                            skill_text.setAttribute("class", "skilltext")
-                            skill_text.innerHTML = "Skills Needed:"
-                            skill_row.append(skill_text)
-                            for (const i in job.Skills) {
-                                let skills_needed = document.createElement("span");
-                                skills_needed.innerText = job.Skills[i]; // Set the default text
+                            // let skill_row = document.createElement("div")
+                            // skill_row.setAttribute("class", "skillrow")
+                            // let skill_text = document.createElement("p")
+                            // skill_text.setAttribute("class", "skilltext")
+                            // skill_text.innerHTML = "Skills Needed:"
+                            // skill_row.append(skill_text)
+                            // for (const i in job.Skills) {
+                            //     let skills_needed = document.createElement("span");
+                            //     skills_needed.innerText = job.Skills[i]; // Set the default text
                                 
-                                for (const j in userData.Skill) {
-                                    if (userData.Skill[j].toLowerCase() === job.Skills[i].toLowerCase()) {
-                                        skills_needed.setAttribute("class", "btn btn-success test");
-                                        break; // Skill matched, no need to continue checking
-                                    } else {
-                                        skills_needed.setAttribute("class", "btn btn-secondary test disabled");
-                                    }
-                                }
+                            //     for (const j in userData.Skill) {
+                            //         if (userData.Skill[j].toLowerCase() === job.Skills[i].toLowerCase()) {
+                            //             skills_needed.setAttribute("class", "btn btn-success test");
+                            //             break; // Skill matched, no need to continue checking
+                            //         } else {
+                            //             skills_needed.setAttribute("class", "btn btn-secondary test disabled");
+                            //         }
+                            //     }
                                 
-                                skill_row.append(skills_needed);
-                            }
+                            //     skill_row.append(skills_needed);
+                            // }
                                                     
                             
-                            jobCard.appendChild(skill_row);
+                            // jobCard.appendChild(skill_row);
                             jobCard.appendChild(detailsbtn);
 
                             jobsContainer.append(jobCard)
--- Login UI/login.js
+++ Login UI/login.js
@@ -1,59 +1,49 @@
 import app from '../config/newconfig.js';
-import { getDatabase, set, ref, update, get, child, push } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, browserSessionPersistence, setPersistence} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
-import { getStorage, ref as sRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-storage.js";
+import FirebaseService from '../config/firebaseService.js';
 
-const database = getDatabase(app);
-const auth = getAuth(app);
-const storage = getStorage()
-const storageref = sRef(storage);
+const firebaseService = new FirebaseService(app)
 
-let login = document.getElementById('submit')
+let login = document.getElementById('submit');
 
 if (login != null) {
-    login.addEventListener("click", async function() { // Note the 'async' keyword
-        const inputEmail = document.getElementById('email').value;
-        const inputPassword = document.getElementById('password').value;
-        console.log(inputEmail);
-        console.log(inputPassword);
+  login.addEventListener("click", async function () {
+    const inputEmail = document.getElementById('email').value;
+    const inputPassword = document.getElementById('password').value;
+    console.log(inputEmail);
+    console.log(inputPassword);
 
-        if (inputEmail !== "" && inputPassword !== "") {
-            try {
-                const userCredential = await signInWithEmailAndPassword(auth, inputEmail, inputPassword);
-                // Handle successful login (userCredential.user contains the user object)
-                const user = userCredential.user;
-                console.log(user);
-                console.log("User signed in:", user);
+    if (inputEmail !== "" && inputPassword !== "") {
+      try {
+        await firebaseService.signInWithEmailAndPassword(inputEmail, inputPassword);
 
-                // Continue with any other actions after successful login
-                onAuthStateChanged(auth, (user) => {
-                    if (user) {
-                      // User is signed in, redirect to authenticated page
-                      setPersistence(auth, browserSessionPersistence)
-                      window.location.href = '/Staff UI/job_listing.html';
-                    } else {
-                      // User is signed out, redirect to login page
-                      console.log("user is not signed in");
-                    }
-                  });
-                
-            } catch (error) {
-                // Handle login failure and show an alert with the error message
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.error("Login failed with error code:", errorCode);
-                console.error("Error message:", errorMessage);
-                alert("Login failed. Please check your email and password.");
+        // Use onAuthStateChanged to redirect based on user role
+        firebaseService.onAuthStateChanged(async function (user) {
+          if (user) {
+            const userUID = user.uid;
+            console.log(userUID)
+            const user_ref = 'Staff/' + userUID;
+            const userData = await firebaseService.getDatabaseValue(user_ref);
+            if (userData) {
+              const user_accesss = userData.Access_Rights;
+              if (user_accesss == 1) {
+                window.location.href = '../Staff UI/job_listing.html';
+              } else {
+                window.location.href = '../Admin UI/job_listing.html';
+              }
+            } else {
+              console.log("User data not found");
             }
-        } else {
-            alert("Both fields are required!");
-        }
-    });
+          } else {
+            console.log("User is not signed in");
+          }
+        });
+      } catch (error) {
+        // Handle login failure and show an alert with the error message
+        console.error("Login failed:", error);
+        alert("Login failed. Please check your email and password.");
+      }
+    } else {
+      alert("Both fields are required!");
+    }
+  });
 }
-
-
-
-
-
-
-
