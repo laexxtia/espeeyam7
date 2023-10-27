4cf08569ae916aee707f53d16b3188d8effb4ec3
Xin Wei <xinweisze@gmail.com>
2023-10-27 15:44:08 +0800

    Updated Server.js

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+
+
+// Serve static files from the respective folders
 app.use('/admin', express.static(path.join(__dirname, 'Admin UI')));
 app.use('/login', express.static(path.join(__dirname, 'Login UI')));
 app.use('/staff', express.static(path.join(__dirname, 'Staff UI')));
 app.use('/config', express.static(path.join(__dirname, 'config')));
 
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'Login UI', 'login.html'));
+//   });
+  
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log('Server is running on http://localhost:3000/login/login.html');
 });
+
