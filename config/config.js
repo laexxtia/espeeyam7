32fa037e84031141c332f037f0eed4e29b12bebb
Zhu Yanfei, Jaslyn <144901628+jakeluvme@users.noreply.github.com>
2023-11-01 19:07:24 +0800

    add config.js

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC5FA8JwfCEvT4DkP2ceJ0ifU0bNbHFAFE",
+    authDomain: "spmg6t7.firebaseapp.com",
+    databaseURL: "https://spmg6t7-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "spmg6t7",
+    storageBucket: "spmg6t7.appspot.com",
+    messagingSenderId: "630427865951",
+    appId: "1:630427865951:web:af42ae53e98566783f9cea"
+  };
+
+  export default firebaseConfig
