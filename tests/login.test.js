edac0a83a83a3ee18f00402d2d4b739f8233b0c7
Xin Wei <xinweisze@gmail.com>
2023-11-06 20:00:06 +0800

    create test case for logging in

---
+++ tests/login.test.js
@@ -0,0 +1,42 @@
+const { chromium } = require('playwright');
+
+describe('Login Test', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await chromium.launch({
+      headless: false, // Set headless to false to run in non-headless mode
+    });
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  test('Login Test', async () => {
+    await page.goto('http://localhost:3000/login/login.html'); // Replace with the URL of the page you want to test.
+    let prevURL = page.url();
+    await page.fill('#email', 'Sally.Loh@allinone.com.sg');
+    await page.fill('#password', '123456');
+
+    // Your Playwright test code here
+    await page.click('#submit');
+    await page.waitForTimeout(3000);
+    let currentURL = page.url();
+    console.log(currentURL);
+
+    // Assertions can be added here to determine if the login was successful or not.
+    if (prevURL == currentURL) {
+      console.log('Login Failed');
+      throw new Error('Login Failed'); // This will fail the test
+    } else {
+      console.log('Login successful');
+    }
+  });
+});
+
+
+
+
